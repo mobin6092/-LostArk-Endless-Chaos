71dc1f2ba6c4206aaf7e2d6b21548076c897daff
mobin6092 <38402338+mobin6092@users.noreply.github.com>
2024-03-19 14:25:33 +0330

    Add files via upload

---
+++ characters.py
@@ -0,0 +1,42 @@
+characters = [
+    {
+        "index": 0,
+        "class": "Bard",
+        "ilvl-endless": 1370,
+        "ilvl-aor": 1400,
+        "lopang": True,
+        "guildDonation": False,
+        "rapport": False,
+    },
+
+    {
+        "index": 1,
+        "class": "Wardancer",
+        "ilvl-endless": 1400,
+        "ilvl-aor": 1400,
+        "lopang": True,
+        "guildDonation": False,
+        "rapport": False,
+    },
+    {
+        "index": 2,
+        "class": "Sh",
+        "ilvl-endless": 1415,
+        "ilvl-aor": 1400,
+        "lopang": True,
+        "guildDonation": False,
+        "rapport": False,
+
+    },
+    {
+        "index": 3,
+        "class": "Glaivier",
+        "ilvl-endless": 1490,
+        "ilvl-aor": 1520,
+        "lopang": True,
+        "guildDonation": False,
+        "rapport": False,
+    },
+
+]
+
